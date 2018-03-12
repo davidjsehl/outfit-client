6cb1a37d578e2e66c800e735ea2d90963199dc97
David Sehl <davidjsehl@gmail.com>
2018-03-11 23:18:02 -0400

    auth navigation flow wired up

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import { Provider } from 'react-redux'
 import { StyleSheet, Text, View } from 'react-native';
-import { SignedOut } from './src/router.js'
+// import { LoggedOut } from './src/router.js'
+import { createRootNavigator } from './src/router';
 import Wardrobe from './src/screens/Wardrobe'
 import store from './src/store'
+import { isLoggedIn } from './src/auth'
 
 
 
 export default class App extends React.Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      loggedIn: false,
+      checkedLogin: false
+    }
+  }
+
+  componentWillMount () {
+    isLoggedIn()
+    .then(res => this.setState({ loggedIn: res, checkedLogin: true}))
+    .catch(err => console.error(err))
+  }
+
   render() {
+    const { loggedIn, checkedLogin } = this.state
+    if (!checkedLogin) {
+      return null
+    }
+
+    const Layout = createRootNavigator(loggedIn)
+
     return (
       <Provider store={store}>
-
-        <Wardrobe />
+        <Layout />
+        {/* <Wardrobe /> */}
       </Provider>
       // <View style={styles.container}>
       //   <Text>Open up App.js to start working on your app!</Text>
